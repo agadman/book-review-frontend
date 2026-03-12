94490d826771ee4efb640b70fb79378f7046e55e
Annika Gadman <annika.gadman@gmail.com>
2026-03-12 12:59:16 -0500

    added fetch Google api to App.tsx just to see that it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,74 @@
+import { useEffect, useState } from "react";
 
-function App() {
-
-  return (
-    <>
-      <h1>Book Review App</h1>
-    </>
-  )
+interface Book {
+  id: string;
+  title: string;
+  authors: string[];
+  thumbnail?: string;
 }
 
-export default App
+function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+      try {
+        setLoading(true);
+        const resp = await fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter");
+
+        if(!resp.ok){
+          throw Error;
+        } else {
+          const data = await resp.json();
+
+          const mappedBooks: Book[] = data.items.map((item: any) => ({
+            id: item.id,
+            title: item.volumeInfo.title,
+            authors: item.volumeInfo.authors || ["Unknown"],
+            thumbnail: item.volumeInfo.imageLinks?.thumbnail
+          }));
+
+          setBooks(mappedBooks);
+          setError(null);
+
+        }
+      } catch (error) {
+        console.log(error);
+        setError("Något gick fel när böckerna skulle hämtas.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+  return (
+    <div>
+      <h1>Book Review App</h1>
+      {
+        error && <p>{error}</p>
+      }
+
+      {
+        loading && <p>Laddar böcker...</p>
+      }
+      
+      <div>
+        {books.map((book) => (
+          <section key={book.id}>
+            <h2>{book.title}</h2> — <p>{book.authors.join(", ")}</p>
+            {book.thumbnail && <br />}
+            {book.thumbnail && (
+              <img src={book.thumbnail} alt={book.title} />
+            )}
+          </section>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
